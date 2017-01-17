41c87e6085245c7eff0fedd81ac55e5220427152
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-16 19:24:35 -0800

    Modified action types

--- app/actions/types.js
+++ app/actions/types.js
@@ -3,3 +3,9 @@ export const SET_SEARCHED_RECIPES = 'SET_SEARCHED_RECIPES';
 export const ADD_RECIPE = 'ADD_RECIPE';
 
 export const GET_PLACES = 'GET_PLACES';
+
+export const GET_IMAGES = 'GET_IMAGES';
+
+export const NEW_PLACES = 'GET_PLACES';
+
+export const NEW_IMAGES = 'NEW_IMAGES';
