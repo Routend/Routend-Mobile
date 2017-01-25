162570dd5b9c67f9dc8aae7ced5c485f0a273b4d
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-25 09:12:16 -0800

    Code clean up on match details

--- app/screens/MatchDetails.js
+++ app/screens/MatchDetails.js
@@ -5,16 +5,18 @@ import {
   Text,
   View,
   Dimensions,
+  TouchableOpacity,
+  Image,
+  AlertIOS,
 } from 'react-native';
 import { Avatar, ArticleText, Divider, Button } from 'react-native-uikit';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators } from '../actions';
+import Router from '../navigation/Router';
+import styles from '../stylesheets/DetailStyles.js';
 
-// {this.props.route.params.name}
-
-export default class MatchDetails extends React.Component {
-  // state = {
-  //   fontLoaded: false,
-  // };
-
+class MatchDetails extends React.Component {
   static route = {
     navigationBar: {
       title: (<Text></Text>),
@@ -22,64 +24,73 @@ export default class MatchDetails extends React.Component {
     },
   }
 
-  // componentDidMount() {
-  // }
+  componentWillMount() {
+    console.log('will', this.props)
+  }
+
+  addMatch() {
+    var that = this;
+    this.props.postUserMatch(this.props.route.params.currUserId, this.props.route.params.matchUserId)
+    .then(function() {
+      AlertIOS.alert('Success', 'Your match has been added!');
+      that.props.navigator.replace(Router.getRoute('matches'));
+    })
+  }
 
   render() {
     return (
       <ScrollView
         style={styles.container}
         >
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-          <Text style={{top: (Dimensions.get('window').width * 0.02), fontSize: 15, color: '#404d5b', fontWeight: 'bold'}}>YOU'VE MATCHED WITH SOMEONE!</Text>
+        <View style={styles.mainView}>
+          <Text style={styles.matchText}>YOU'VE MATCHED WITH SOMEONE!</Text>
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center', top: (Dimensions.get('window').width * 0.05)}}>
-          <View style={{padding: (Dimensions.get('window').width * 0.026), justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.secondView}>
+          <View style={styles.thirdView}>
             <Avatar
               src={this.props.route.params.currUserImg}
               size={(Dimensions.get('window').width * 0.36)}
               circle={true}
-              onPress={() => console.log('pressed')}
             />
-            <Text style={{fontSize: 13.5, fontWeight: 'bold', opacity: 0.8}}>{this.props.route.params.currUserName}</Text>
+            <Text style={styles.userName}>{this.props.route.params.currUserName}</Text>
           </View>
-          <View style={{padding: (Dimensions.get('window').width * 0.025), justifyContent: 'center', alignItems: 'center'}}>
+          <View style={styles.fourthView}>
             <Avatar
               src={this.props.route.params.image}
               size={(Dimensions.get('window').width * 0.36)}
               circle={true}
-              onPress={() => console.log('pressed')}
             />
-            <Text style={{fontSize: 13.5, fontWeight: 'bold', opacity: 0.8}}>{this.props.route.params.name}</Text>
+            <Text style={styles.userName2}>{this.props.route.params.name}</Text>
           </View>
         </View>
 
-           <View style={{alignItems: 'center', justifyContent: 'center', width: (Dimensions.get('window').width * 0.95),top: (Dimensions.get('window').height * 0.05), borderRadius: 3, backgroundColor: '#fff', borderColor: '#D8D8D8', borderWidth: 1, shadowColor: '#D8D8D8',shadowRadius: 0.03, shadowOpacity: 0.5, shadowOffset: { width: 1, height: 1, }, left: (Dimensions.get('window').width * 0.022)}}>
+           <View style={styles.box}>
            <Divider
             color={'#eee'}
           />
           <ArticleText
             username={this.props.route.params.name}
             text={this.props.route.params.status}
-            // onPress={() => console.log('pressed')}
-            style={{width: (Dimensions.get('window').width * 0.8), justifyContent: 'center'}}
+            style={styles.status}
           />
           <Divider
             color={'#eee'}
           />
           </View>
-          <View style={{top: (Dimensions.get('window').height * 0.060), left: (Dimensions.get('window').width * 0.02)}}>
+          <View style={styles.matchView}>
           <Button
             color={'#fff'}
+            onPress={() => this.addMatch()}
             backgroundColor={'#26a69a'}
-            style={{width: (Dimensions.get('window').width * 0.96)}}
+            style={styles.accept}
             radius={5}>
             Accept
           </Button>
           <Button
             color={'#fff'}
+            onPress={() => this.props.navigator.replace(Router.getRoute('matches'))}
             backgroundColor={'#25272A'}
-            style={{width: (Dimensions.get('window').width * 0.96), bottom: (Dimensions.get('window').height * 0.01)}}
+            style={styles.reject}
             radius={5}>
             Reject
           </Button>
@@ -89,9 +100,13 @@ export default class MatchDetails extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    top: (Dimensions.get('window').height * 0.05)
-  },
-});
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(ActionCreators, dispatch);
+}
+
+function mapStateToProps(state) {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MatchDetails);
