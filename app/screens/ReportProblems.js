1a31799f0affe67710310688937aad3cb861b834
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-24 23:26:20 -0800

    Clean up code in report problems screen

--- app/screens/ReportProblems.js
+++ app/screens/ReportProblems.js
@@ -19,16 +19,13 @@ export default class ReportProblems extends React.Component {
     }
   }
 
-  updateProfile() {
-    // this.props.postProfile(this.state.userId, this.state.firstName, this.state.lastName, this.state.gender, this.state.city, this.state.state, this.state.image, this.state.status)
-    // .done(function() {
-      AlertIOS.alert('Success', 'Your notification settings have been updated!');
-    // })
+  contactUs() {
+    AlertIOS.alert('Contact', 'Message us at support@routend.com!');
   }
 
   static route = {
     navigationBar: {
-      title: (<Text style={{color: 'white', fontSize: 14, fontWeight: 'bold'}}>HELP CENTER</Text>),
+      title: (<Text style={{color: 'white', fontSize: 14, fontWeight: 'bold'}}>REPORT A PROBLEM</Text>),
       backgroundColor: '#175785',
     },
   }
@@ -40,15 +37,15 @@ export default class ReportProblems extends React.Component {
         <Card
           title='Report a Problem'
           image={{uri: 'http://i2.cdn.cnn.com/cnnnext/dam/assets/111110075215-iphone-4s-battery-horizontal-large-gallery.jpg'}}>
-          <Text style={{marginBottom: 10}}>
+          <Text style={styles.text}>
             Have any issues with our app? Let us know!
           </Text>
         </Card>
         <Button
-          onPress={() => this.updateProfile()}
+          onPress={() => this.contactUs()}
           color={'#fff'}
           backgroundColor={'#175785'}
-          style={{position: 'absolute', top: (Dimensions.get('window').height * 0.375), width: (Dimensions.get('window').width * 0.8), left: (Dimensions.get('window').width * 0.1)}}
+          style={styles.button}
           radius={5}>
           Contact Us
         </Button>
@@ -56,3 +53,18 @@ export default class ReportProblems extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    marginBottom: 10
+  },
+  button: {
+    position: 'absolute',
+    top: (Dimensions.get('window').height * 0.375),
+    width: (Dimensions.get('window').width * 0.8),
+    left: (Dimensions.get('window').width * 0.1)
+  }
+});
