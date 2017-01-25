aa7c3700d0094c7042c5d549be5780b7746362bb
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-24 23:26:07 -0800

    Clean up code in signup screen

--- app/screens/SignupScreen.js
+++ app/screens/SignupScreen.js
@@ -9,6 +9,7 @@ import {
   AlertIOS,
 } from 'react-native';
 import Router from '../navigation/Router';
+import styles from '../stylesheets/SignUpStyles.js';
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -51,28 +52,20 @@ export default class Signup extends React.Component {
     return (
        <Image source={require('../assets/Signup.jpg')} style={styles.container}>
        <TextInput
-          style={{position: 'absolute', top: (Dimensions.get('window').height * 0.598), left: (Dimensions.get('window').width * 0.23), borderWidth: 0, color: 'white', height: (Dimensions.get('window').height * 0.05), width: (Dimensions.get('window').width), fontSize: 13}}
+          style={styles.userName}
           onChangeText={(text) => this.setState({username: text})}
           placeholder={'Username'}
           placeholderTextColor={'white'}
         />
        <TextInput
-          style={{position: 'absolute', top: (Dimensions.get('window').height * 0.691), left: (Dimensions.get('window').width * 0.23), borderWidth: 0, color: 'white', height: (Dimensions.get('window').height * 0.05), width: (Dimensions.get('window').width), fontSize: 13}}
+          style={styles.password}
           onChangeText={(text) => this.setState({password: text})}
           placeholder={'Password'}
           placeholderTextColor={'white'}
           password={true}
         />
-        <TouchableOpacity onPress={() => this.createUser(this.state.username, this.state.password) } style={{top: (Dimensions.get('window').height * 0.821), width: (Dimensions.get('window').width), height: (Dimensions.get('window').height * 0.09)}} />
+        <TouchableOpacity onPress={() => this.createUser(this.state.username, this.state.password) } style={styles.submit} />
       </Image>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: null,
-    height: null,
-  }
-});
