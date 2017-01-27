d445b517f98fba4113f53f61c20616a8ef274d8d
Dunstan Zheng <dunstanzheng@gmail.com>
2017-01-26 22:51:13 -0800

    Code clean up in match screen

--- app/screens/MatchScreen.js
+++ app/screens/MatchScreen.js
@@ -11,12 +11,13 @@ import Router from '../navigation/Router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ActionCreators } from '../actions';
+import styles from '../stylesheets/MatchStyles.js';
 
 class MatchScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userId: 2,
+      userId: global.id,
       ready: false,
     }
   }
@@ -29,17 +30,30 @@ class MatchScreen extends React.Component {
   }
 
   componentWillMount() {
-    console.log('will mount', this.props)
     var that = this;
-    // this.props.fetchLocationMatches(this.state.userId)
-    // .then(function() {
+    this.props.fetchLocationMatches(this.state.userId)
+    .then(function() {
       that.props.fetchMatchSuggestions(that.state.userId)
       .then(function() {
-        console.log('mUsers', that.props)
+        var counter = 0;
+        var profiles = [];
+        for(var i = 0; i < that.props.mUsers.length; i++) {
+          fetch(`http://107.170.226.9:3000/profiles?id_users=${that.props.mUsers[i][1]}`)
+          .then((resp) => resp.json())
+          .then(resp => {
+            counter++;
+            if (resp[0] !== undefined) {
+              profiles.push(resp[0]);
+            }
+            if (counter === that.props.mUsers.length) {
+              that.state.totalMatches = profiles;
                 that.setState({
                 ready: true,
               })
-          // })
+            }
+          })
+        }
+      })
     })
   }
 
@@ -53,36 +67,36 @@ class MatchScreen extends React.Component {
       <ScrollView
         style={styles.container}
         contentContainerStyle={this.props.route.getContentContainerStyle()}>
-        <Text style={{left: (Dimensions.get('window').width * 0.03), top: (Dimensions.get('window').width * 0.02), fontSize: 13, color: '#404d5b', fontWeight: 'bold'}}>NEW MATCHES</Text>
-        <View style={{flexDirection: 'row', justifyContent: 'center', top: (Dimensions.get('window').width * 0.04)}}>
+        <Text style={styles.newMatch}>NEW MATCHES</Text>
+        <View style={styles.mainView}>
         {
-            this.props.mUsers.map((l, i) => (
-            <View key={i} style={{padding: 5, justifyContent: 'center', alignItems: 'center'}}>
+            this.state.totalMatches.map((l, i) => (
+            <View key={i} style={styles.matches}>
               <Avatar
                 src={l.image}
                 size={(Dimensions.get('window').width * 0.21)}
                 circle={true}
-                onPress={() => { this.props.navigator.push(Router.getRoute('matchdetails', {name: (l.first_name + ' ' + l.last_name.substr(0, 1) + '.'), status: l.status, image: l.image, currUserName: this.props.currUser.first_name, currUserImg: this.props.currUser.image}))}}
+                onPress={() => { this.props.navigator.replace(Router.getRoute('matchdetails', {name: (l.first_name + ' ' + l.last_name.substr(0, 1) + '.'), status: l.status, image: l.image, currUserName: this.props.currUser.first_name, currUserId: this.props.currUser.id_users, currUserImg: this.props.currUser.image, matchUserId: l.id_users}))}}
               />
-              <Text style={{fontSize: 12, fontWeight: 'bold', opacity: 0.8}}>{l.first_name}</Text>
+              <Text style={styles.firstName}>{l.first_name}</Text>
             </View>
             ))
         }
         </View>
-        <View style={{top: (Dimensions.get('window').width * 0.05)}}>
-        <Text style={{left: (Dimensions.get('window').width * 0.03), top: (Dimensions.get('window').width * 0.005), fontSize: 13, color: '#404d5b', fontWeight: 'bold'}}>ACTIVITY SUGGESTIONS</Text>
+        <View style={styles.locationMatch}>
+        <Text style={styles.activityText}>ACTIVITY SUGGESTIONS</Text>
         {
             this.props.mLocations.map((l, i) => (
-            <View key={i} style={{padding: (Dimensions.get('window').width * 0.03)}}>
+            <View key={i} style={styles.location}>
               <ReviewCell
                 title={l.name}
                 description={l.address}
-                src={(l.image || 'http://assets.nydailynews.com/polopoly_fs/1.1369168.1370966034!/img/httpImage/image.jpg_gen/derivatives/article_750/transformers.jpg')}
-                onPress={() => { this.props.navigator.push(Router.getRoute('locationdetails', {placeId: l.placeId})) }}
+                src={(l.image || 'https://techpur.com/wp-content/plugins/facebook-share-like-popup-viralplus/default.jpg')}
+                onPress={() => { this.props.navigator.replace(Router.getRoute('locationdetails', {placeId: l.placeId, currUserId: this.props.currUser.id_users})) }}
                 rating={0}
                 outOf={0}
               />
-              <RatingBox rating={l.rating} outOf={5} style={{position: 'absolute', bottom: (Dimensions.get('window').height * 0.01), left: (Dimensions.get('window').width * 0.025)}}/>
+              <RatingBox rating={l.rating} outOf={5} style={styles.locationStar}/>
             </View>
             ))
           }
@@ -92,12 +106,6 @@ class MatchScreen extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(ActionCreators, dispatch);
